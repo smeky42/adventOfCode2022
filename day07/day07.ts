a91c9064dd13b32a8b13b1085006cd795ca30f94
Peter Neubauer <peter.neubauer@methodpark.de>
2022-12-07 12:00:32 +0100

    day07 filesize part 1

---
+++ day07/day07.ts
@@ -0,0 +1,80 @@
+import * as fs from 'fs';
+
+const input = fs.readFileSync('input.txt','utf8');
+
+let path: string = "/";
+
+function startsWithNumber(str: string) {
+    return /^\d/.test(str);
+}
+
+
+var folderSum: number = 0;
+let folderSize = new Map<string,number>([]);
+input.split(/\r?\n/).forEach(line =>  {
+    
+    if(line.startsWith("$ ls")) {
+        folderSum = 0;
+    }
+
+    if(startsWithNumber(line)) {
+        var size: number = +line.split(" ")[0];
+        // console.log("+" + size);
+        folderSum += size;
+    }
+
+    if(line.startsWith("$ cd")) {
+        if(folderSum > 0) {
+            // console.log(path + " " + folderSum );
+            folderSize.set(path, folderSum);
+            folderSum = 0;
+        }
+
+        if(line == "$ cd ..") {
+            path = path.substring(0, path.lastIndexOf("/"));//replace(/\/[^\/]+$/,"");
+        } else if(line == "$ cd /") {
+            path = "";
+        } else {
+            var newFolder: string = line.replace("$ cd ","");
+            path += "/" + newFolder ;
+            if(!folderSize.has(path)){
+                folderSize.set(path, 0);
+            }
+        }
+        // console.log(path);
+    };
+
+
+});
+
+
+let folderSumSize = new Map<string,number>([]);
+
+folderSize.forEach( (size, folder) => {
+    let folderSum: number = size; 
+
+    // console.log("-->" + folder);
+    // console.log("+" + size);
+    folderSize.forEach( (innerSize, innerFolder) => {
+        if(innerFolder.includes(folder + "/")) {
+            // console.log(innerFolder + " is part of " + folder);
+            // console.log(folderSum + "+" + innerSize);
+            folderSum += innerSize;
+            // console.log(folderSum);
+        }
+    });
+
+    // console.log("=" + folderSum);
+    folderSumSize.set(folder, folderSum);
+
+} ); 
+
+var sum: number = 0;
+folderSumSize.forEach((size, folder) => {
+    if(size <= 100000) {
+        console.log(folder + " " + size);
+        sum += size; 
+    }
+});  
+
+console.log(sum);
