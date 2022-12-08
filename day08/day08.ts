c863a5e11f69a8e1c3c245548eaaf0d5215a2762
Peter Neubauer <peter.neubauer@methodpark.de>
2022-12-08 09:23:18 +0100

    day08 treehouse part 2

--- day08/day08.ts
+++ day08/day08.ts
@@ -6,60 +6,68 @@ const input = fs.readFileSync('input.txt','utf8');
 
 function hasBiggerTreeAbove(forest: number[][], row: number, column: number) {
 
+    var sum: number = 0;
     let height: number = forest[row][column];
-    for(let i = 0; i < row; i++) {
+    for(let i = row - 1; i >= 0; i--) {
         // console.log(forest[i][column]);
+        sum+=1;
         if(height <= forest[i][column]) {
-            return true; 
+            return sum; 
         }
     }
 
-    return false;
+    return sum;
 }
 
 function hasBiggerTreeBelow(forest: number[][], row: number, column: number) {
 
+    var sum: number = 0;
     let height: number = forest[row][column];
     for(let i = row +1; i < forest.length; i++) {
         // console.log(forest[i][column]);
+        sum+=1;
         if(height <= forest[i][column]) {
-            return true; 
+            return sum; 
         }
     }
 
-    return false;
+    return sum;
 }
 
 function hasBiggerTreeLeft(forest: number[][], row: number, column: number) {
 
+    var sum: number = 0;
     let height: number = forest[row][column];
-    for(let i = 0; i < column; i++) {
+    for(let i = column-1; i >= 0; i--) {
         // console.log(forest[row][i]);
+        sum+=1;
         if(height <= forest[row][i]) {
-            return true; 
+            return sum; 
         }
     }
 
-    return false;
+    return sum;
 }
 
 function hasBiggerTreeRight(forest: number[][], row: number, column: number) {
-
+   
+    var sum: number = 0;
     let height: number = forest[row][column];
     for(let i = column +1; i < forest[row].length; i++) {
         // console.log(forest[row][i]);
+        sum+=1;
         if(height <= forest[row][i]) {
-            return true; 
+            return sum; 
         }
     }
 
-    return false;
+    return sum;
 }
 
 function hasBiggerTreeAround(forest: number[][], row: number, column: number) {
-    return hasBiggerTreeAbove(forest, row, column) &&
-            hasBiggerTreeBelow(forest, row, column) &&
-            hasBiggerTreeLeft(forest, row, column) &&
+    return hasBiggerTreeAbove(forest, row, column) *
+            hasBiggerTreeBelow(forest, row, column) *
+            hasBiggerTreeLeft(forest, row, column) *
             hasBiggerTreeRight(forest, row, column) 
 }
 
@@ -71,15 +79,16 @@ input.split(/\r?\n/).forEach(line =>  {
 });
 
 
-var sum: number = 0;
+var sum: number[] = new Array();
 console.log("Rows: " + forest.length);
 console.log("Columns: " + forest[0].length);
 
 for(let row = 0; row < forest.length; row++) {
     for(let column = 0; column < forest[row].length; column++) {
-        if(!hasBiggerTreeAround(forest, row, column)) {
-            sum += 1;
-        }
+        var treeSum: number = hasBiggerTreeAround(forest, row, column);
+        console.log(treeSum);
+        sum.push(treeSum);
     }
 }
-console.log(sum);
+
+console.log("Best Treehouse: " + Math.max.apply(null,sum));
