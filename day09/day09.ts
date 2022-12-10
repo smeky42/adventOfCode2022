de0d25456a07d4d9e0f66734495a128a56487da7
Peter Neubauer <development@smeky.de>
2022-12-10 10:33:00 +0100

    day09 bridge part 2

--- day09/day09.ts
+++ day09/day09.ts
@@ -1,12 +1,8 @@
 import * as fs from 'fs';
 
-const input = fs.readFileSync('input.txt','utf8');
+const input = fs.readFileSync('inputSmall.txt','utf8');
 
-var hX: number = 0;
-var hY: number = 0; 
-
-var tX: number = 0;
-var tY: number = 0;
+var snake: number[][] = [[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]];
 
 var tVisited = new Set<string>();
 
@@ -15,18 +11,20 @@ function moveH(direction: string, steps: number) {
     // positive x
     if(direction == "R") {
         for(let i = 0; i < steps; i++) {
-            hX +=1; 
-            moveT();
-            console.log( hX + "," + hY + " - " +  tX + "," + tY);
+            snake[0][0] +=1; 
+            moveT(1);
+            // console.log( hX + "," + hY + " - " +  tX + "," + tY);
+            // console.log("+X")
         }
     }
 
     // positive y
     if(direction == "U") {
         for(let i = 0; i < steps; i++) {
-            hY +=1; 
-            moveT();
-            console.log( hX + "," + hY + " - " +  tX + "," + tY);
+            snake[0][1] +=1; 
+            moveT(1);
+            // console.log( hX + "," + hY + " - " +  tX + "," + tY);
+            // console.log("+Y")
 
         }   
     }
@@ -34,30 +32,43 @@ function moveH(direction: string, steps: number) {
     // negative x
     if(direction == "L") {
         for(let i = 0; i < steps; i++) {
-            hX -=1; 
-            moveT();
-            console.log( hX + "," + hY + " - " +  tX + "," + tY);
-
+            snake[0][0] -=1; 
+            moveT(1);
+            // console.log( hX + "," + hY + " - " +  tX + "," + tY);
         }   
     }
 
     // negative y
     if(direction == "D") {
         for(let i = 0; i < steps; i++) {
-            hY -=1; 
-            moveT();
-            console.log( hX + "," + hY + " - " +  tX + "," + tY);
-
+            snake[0][1] -=1; 
+            moveT(1);
+            // console.log( hX + "," + hY + " - " +  tX + "," + tY);
         }       
     }
 
 }
 
-function moveT() {
-    tVisited.add(tX + "," + tY);
+function moveT(bodypart: number) {
+    var hX = snake[bodypart-1][0];
+    var hY = snake[bodypart-1][1];
+
+    var tX = snake[bodypart][0];
+    var tY = snake[bodypart][1];
+
+    // console.log(snake[0][0] + "," + snake[0][1]);
+
+    // console.log("T  "+ bodypart + " " + tX + "," + tY);
+    
     var betweenX: number = hX-tX;
     var betweenY: number = hY-tY;
 
+    // console.log("Abstand X: " + betweenX + " Y:" + betweenY);
+
+    if(bodypart == 8 ) { 
+        console.log("Visited: " + tX + "," + tY)
+        tVisited.add(tX + "," + tY);
+    }
 
     if(Math.abs(betweenX) <= 1 && Math.abs(betweenY) <= 1) {
         return; 
@@ -78,6 +89,14 @@ function moveT() {
     if(betweenY < 0) {
         tY -=1;
     }
+
+    snake[bodypart][0] = tX;
+    snake[bodypart][1] = tY;
+
+
+    if(bodypart < 8) {
+        moveT(bodypart+1);
+    }
 }
 
 
@@ -88,9 +107,10 @@ input.split(/\r?\n/).forEach(line =>  {
 
     console.log(direction + "->" + steps);
     moveH(direction, steps);
-    console.log("H:" + hX + "," + hY);
-    console.log("T:" + tX + "," + tY);
+    console.log(snake);
+    // console.log("H:" + hX + "," + hY);
+    // console.log("T:" + tX + "," + tY);
     
 });
-
+console.log(tVisited);
 console.log(tVisited.size)
